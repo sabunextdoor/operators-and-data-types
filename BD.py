d23dd53a64a783c5c779997ac8a52051a64b1c51
sabunextdoor <sabuthwala@gmail.com>
2024-10-28 19:55:43 +0200

    Pythons

---
+++ BD.py
@@ -0,0 +1,12 @@
+a=12
+b=2
+print("sumis",(b+b))
+print("productis",(b**b))
+print("quotientis",(a/b))
+print("differenceis",(a-b))
+
+print("modulus",(a%b))
+print("floor division",(a//b))
+print("equal",(a==b))
+print("not equal",(a!=b))
+print("greater than",(a>b))
---
+++ SD.py
@@ -0,0 +1,10 @@
+name="Sabu"
+age=14
+isstudent=True
+weight=50.67
+print("the datatype of nameis",type(name))
+print("the datatype of nameis",type(age))
+print("the datatype of nameis",type(isstudent))
+print("the datatype of nameis",type(weight))
+weight=int(weight)
+print("the new data type of weight is",type(weight))
